0a6468e92679994fed3118c2cfd9c44f95611673
priyanshu31 <priyanshu@github.com>
2021-06-03 17:17:35 +0530

    Added Date Feature

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-let notetitle = localStorage.getItem("notetitle");
-let notetext = localStorage.getItem("notetext");
-if (notetitle == null)
-    notetext = [], notetitle = [];
-else
-    notetext = JSON.parse(notetext), notetitle = JSON.parse(notetitle);
-
+let notetext, notetitle, date;
 displaynotes();
 
 let savebtn = document.getElementById('savebtn');
 savebtn.addEventListener('click', () => {
+    notetitle = localStorage.getItem("notetitle");
+    notetext = localStorage.getItem("notetext");
+    date = localStorage.getItem("date");
 
+    if (notetitle == null)
+        notetext = [], notetitle = [], date = [];
+    else
+        notetext = JSON.parse(notetext), notetitle = JSON.parse(notetitle), date = JSON.parse(date);
 
     let text = document.getElementById('addtext');
     let title = document.getElementById('notetitle');
@@ -17,6 +18,9 @@ savebtn.addEventListener('click', () => {
     if (title.value == "")
         return;
 
+    let d = new Date();
+    d = d.toDateString();
+    date.push(d);
     notetitle.push(title.value);
     notetext.push(text.value);
     title.value = text.value = "";
@@ -24,6 +28,7 @@ savebtn.addEventListener('click', () => {
     localStorage.clear();
     localStorage.setItem('notetitle', JSON.stringify(notetitle));
     localStorage.setItem('notetext', JSON.stringify(notetext));
+    localStorage.setItem('date', JSON.stringify(date));
 
     displaynotes();
 });
@@ -32,25 +37,27 @@ function displaynotes() {
 
     notetitle = localStorage.getItem("notetitle");
     notetext = localStorage.getItem("notetext");
+    date = localStorage.getItem("date");
+
     if (notetitle == null)
         notetext = [], notetitle = [];
     else
-        notetext = JSON.parse(notetext), notetitle = JSON.parse(notetitle);
+        notetext = JSON.parse(notetext), notetitle = JSON.parse(notetitle), date = JSON.parse(date);
 
     let html = "";
     notetext.forEach((element, index) => {
-        html += `<div class="card my-2 mx-2" style="width: 18rem;">
-        <div class="card-body">
+        html += `<div class="card border-dark mb-3" style="max-width: 18rem; padding: 0">
+        <div class="card-header">${date[index]}</div>
+        <div class="card-body text-dark">
           <h5 class="card-title">${notetitle[index]}</h5>
           <p class="card-text">${element}</p>
           <a href="#" id="${index}" onclick = "deletenote(this.id)" class="btn btn-primary">Delete Note</a>
-        </div>
-      </div>`
+        </div>`;
     });
-
+    
     if (html == "")
-        html = 'No notes added. Add notes using "Add a Note section"';
-
+    html = 'No notes added. Add notes using "Add a Note section"';
+    
     document.getElementById('notes').innerHTML = html;
 }
 
@@ -91,18 +98,18 @@ search.addEventListener('input', () => {
 
     notetext.forEach((element, index) => {
         if (notetitle[index].toLowerCase().includes(searchval) || element.toLowerCase().includes(searchval)) {
-            html += `<div class="card my-2 mx-2" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">${notetitle[index]}</h5>
-            <p class="card-text">${element}</p>
-            <a href="#" id="${index}" onclick = "deletenote(this.id)" class="btn btn-primary">Delete Note</a>
-            </div>
+            html += `<div class="card border-dark mb-3" style="max-width: 18rem; padding: 0">
+            <div class="card-header">${date[index]}</div>
+            <div class="card-body text-dark">
+              <h5 class="card-title">${notetitle[index]}</h5>
+              <p class="card-text">${element}</p>
+              <a href="#" id="${index}" onclick = "deletenote(this.id)" class="btn btn-primary">Delete Note</a>
             </div>`;
         }
     });
-
+    
     if (html == "")
-        html = `${search.value} doesn't exist`;
-
+    html = `${search.value} doesn't exist`;
+    
     document.getElementById('notes').innerHTML = html;
 });
